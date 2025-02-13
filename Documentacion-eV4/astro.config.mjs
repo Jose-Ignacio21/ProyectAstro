b22eecf4c7d4fd281302556d072e85fb98f4dab7
Jose-Ignacio21 <jose.garcia.cruz@ieslaarboleda.es>
2025-02-13 18:23:01 +0100

    Cambio titulo barra de navegacion

--- Documentacion-eV4/astro.config.mjs
+++ Documentacion-eV4/astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Software - Manual',
+			title: 'Software eV4',
 			logo: {
 				src: './src/assets/logo-ev4.webp',
 			},
