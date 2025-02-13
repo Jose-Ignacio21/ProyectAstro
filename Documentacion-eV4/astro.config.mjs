4fa4b5c14067897347d2193ba4c6e1cc4f9336d3
DanMack03 <daniel345518112003@gmail.com>
2025-02-13 14:43:32 +0100

    Limpieza

--- Documentacion-eV4/astro.config.mjs
+++ Documentacion-eV4/astro.config.mjs
@@ -12,17 +12,6 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
 					label: 'Primeros pasos',
 					autogenerate: { directory: 'primerospasos' },
 				},
--- Documentacion-eV4/src/content/docs/documentacion/pagina1.md
+++
@@ -1,8 +0,0 @@
----
-title: Prueba 1
-description: Primera página
----
-
-## Probando a meter nuevas páginas
-
-En Starlight se pueden estructurar todo tipo de documentos.
